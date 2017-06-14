4c2f0c3490a22e256862524a8b792bea5fb7f2d4
wangyang <wangyang@vmobel.com>
2017-06-14 10:41:13 +0800

    react-demo

--- components/test.js
+++ components/test.js
@@ -3,11 +3,21 @@ const React = require('react');
 class Test extends React.Component {
   constructor(...args) {
     super(...args);
+    this.state = {
+      msg:'how are you'
+    };
+  }
+  talk(){
+    this.setState({
+      msg:'I am fine'
+    });
   }
 
   render() {
     return <div>
     	<h1>hello {this.props.name}</h1>
+      <h2>{this.state.msg}</h2>
+      <button onClick={this.talk.bind(this)}>点击进行对话</button>
     </div>;
   }
 }
